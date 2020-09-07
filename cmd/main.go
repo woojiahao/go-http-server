af0801617c241566f39f0f0654e685bbd42f460e
woojiahao <woojiahao1234@gmail.com>
2020-09-07 20:53:54 +0800

    Use default OS threading for conn management
    Replace channel approach

--- cmd/main.go
+++ cmd/main.go
@@ -72,23 +72,8 @@ func (s *Server) handleConn(c *Client) {
 	}
 }
 
-// Monitors the backlog of the server. Every time a new connection comes in and the backlog is not full, the connection
-// will be picked off from the channel and processed. A new client is created and added to the server to manage.
-func monitorBacklog(server *Server, backlog <-chan net.Conn) {
-	for {
-		conn := <-backlog
-		client := server.addConn(conn)
-		fmt.Printf("New connection made to client %s\n", client.id)
-		go server.handleConn(client)
-	}
-}
-
 func main() {
 	server := createServer(8000, 1)
-	// Create a backlog to house the pending connection requests that can be held at once
-	backlog := make(chan net.Conn, server.backlog)
-
-	go monitorBacklog(server, backlog)
 
 	for {
 		conn, err := server.ln.Accept()
@@ -96,13 +81,8 @@ func main() {
 			panic(err)
 		}
 
-		select {
-		case backlog <- conn:
-		default:
-			// If the channel is already full
-			fmt.Println("Connection rejected")
-			conn.Write([]byte("Queue is full!"))
-			conn.Close()
-		}
+		client := server.addConn(conn)
+		fmt.Printf("New connection made to client %s\n", client.id)
+		go server.handleConn(client)
 	}
 }
