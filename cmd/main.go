4f55f09508986392c7957707a4100f612b92e835
woojiahao <woojiahao1234@gmail.com>
2020-09-26 21:55:43 +0800

    Add basic CLI + read from config file

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,71 @@
 package main
 
 import (
+	"fmt"
+	"github.com/urfave/cli/v2"
 	"github.com/woojiahao/go-http-server/internal/server"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	s := server.Create(8000, "/home/chill/dotfiles")
-	s.Start()
+	var port int
+	var path, config string
+	currentDir, _ := os.Getwd()
+
+	app := &cli.App{
+		Name:  "go-http",
+		Usage: "starts a simple HTTP server",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:        "port",
+				Value:       8000,
+				Usage:       "port of the HTTP server",
+				Destination: &port,
+			},
+			&cli.StringFlag{
+				Name:        "path",
+				Value:       currentDir,
+				Usage:       "root path of HTTP server to serve documents from",
+				Destination: &path,
+			},
+			&cli.StringFlag{
+				Name:        "config",
+				Value:       "",
+				Usage:       "configuration filename (in YAML)",
+				Destination: &config,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			if strings.TrimSpace(config) != "" {
+				var c *server.Config
+				fmt.Println("Using configuration")
+				yamlFile, err := ioutil.ReadFile(config)
+				if err != nil {
+					log.Fatal(err)
+				}
+				err = yaml.Unmarshal(yamlFile, &c)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				// TODO Handle if blank, use default value instead
+				port = c.Port
+				path = c.Path
+			}
+
+			s := server.Create(port, path)
+			s.Start()
+
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ internal/server/config.go
@@ -0,0 +1,6 @@
+package server
+
+type Config struct {
+	Port int    `yaml:"port"`
+	Path string `yaml:"path"`
+}
